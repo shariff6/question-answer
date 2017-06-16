74303e8575c9dee4107fc178dca712bc797372a7
shariff6 <shariff.mohammed.a@gmail.com>
2017-06-16 17:36:30 +0300

    added if function incase time submission time was not recorded

--- app/helpers/answer-no.js
+++ app/helpers/answer-no.js
@@ -4,7 +4,9 @@ export function answerNo(params) {
   var question = params[0];
   var x = question.get('answers').get('length')
    if(x > 0){
-     return x
+     return '[' + x + 'answers]'
+   }else if (x === 0) {
+     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove-circle"></span>' + "Be the first to answer");
    }
 }
 
