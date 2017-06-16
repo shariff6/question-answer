edd489083ac9690af79e8b86265cc6e1440b0fd9
shariff6 <shariff.mohammed.a@gmail.com>
2017-06-16 17:32:12 +0300

    added computed propert to display who and when the question was submitted

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('question.name','question.date', function () {
+    var y = this.get('question.date')
+    if (y === null) {
+      return "- by " + this.get('question.name')
+    } else {
+
+    return "- by " + this.get('question.name') + " on " + this.get('question.date')
+}
+  })
 });
