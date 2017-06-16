e6f932774d532cadf32602be63d2462f86338a42
shariff6 <shariff.mohammed.a@gmail.com>
2017-06-16 11:52:02 +0300

    added rating attribute to answers

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  name: DS.attr(),
   answer: DS.attr(),
   question: DS.belongsTo('question', { async: true }),
+  rating: DS.attr()
 });
